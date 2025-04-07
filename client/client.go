002be163a4fe312883fe9f095971965075388e56
krokhalev <cramush@gmail.com>
2025-04-08 01:20:28 +0300

    Update client - add tun interface init, add tun traffic reader and parser.

--- client/client.go
+++ client/client.go
@@ -1,40 +1,89 @@
 package main
 
 import (
+	"Repo/protocols"
 	"fmt"
+	"github.com/songgao/water"
+	"log"
 	"net"
+	"os/exec"
 )
 
 func main() {
-
-	conn, err := net.Dial("tcp", "localhost:2411")
+	//ifaces, err := net.Interfaces()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//for _, iface := range ifaces {
+	//	fmt.Println(iface)
+	//	if iface.Flags&net.FlagPointToPoint != 0 {
+	//		fmt.Printf("Point-to-Point interface: %s\n", iface.Name)
+	//	}
+	//}
+	// создаем tun интерфейс
+	ifce, err := water.New(water.Config{
+		DeviceType: water.TUN,
+	})
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	defer conn.Close()
+	log.Println("Interface Name:", ifce.Name())
+
+	// назначаем ip для интерфейса
+	cmd := exec.Command("ip", "addr", "add", "10.0.0.1/24", "dev", ifce.Name())
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("setting address for interface error: %s", string(out))
+		log.Fatal(err)
+	}
+
+	// поднимаем интерфейс
+	cmd = exec.Command("ip", "link", "set", "dev", ifce.Name(), "up")
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("up interface error: %s", string(out))
+		log.Fatal(err)
+	}
+
+	// назначаем дефолтный роут через интерфейс
+	cmd = exec.Command("ip", "route", "add", "default", "via", "10.0.0.1", "dev", ifce.Name())
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("set new interface as default error: %s", string(out))
+		log.Fatal(err)
+	}
+
+	// направляем запросы первой половины IP-адресов в интерфейс (0.0.0.0 — 127.255.255.255)
+	cmd = exec.Command("ip", "route", "add", "0.0.0.0/1", "dev", ifce.Name())
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("setting route traffic range 1 error: %s", string(out))
+		log.Fatal(err)
+	}
+
+	// направляем запросы первой половины IP-адресов в интерфейс (128.0.0.0 — 255.255.255.255)
+	cmd = exec.Command("ip", "route", "add", "128.0.0.0/1", "dev", ifce.Name())
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("setting route traffic range 2 error: %s", string(out))
+		log.Fatal(err)
+	}
+
+	log.Println("Interface configured")
+
+	packet := make([]byte, 1500) // MTU буфер
+
 	for {
-		var source string
-		fmt.Print("Введите слово: ")
-		_, err := fmt.Scanln(&source)
+		n, err := ifce.Read(packet)
 		if err != nil {
-			fmt.Println("Некорректный ввод", err)
-			continue
+			log.Fatal(err)
 		}
-		// отправляем сообщение серверу
-		n, err := conn.Write([]byte(source))
-		if n == 0 || err != nil {
-			fmt.Println(err)
-			return
-		}
-		// получем ответ
-		fmt.Print("Перевод:")
-		buff := make([]byte, 1024)
-		n, err = conn.Read(buff)
-		if err != nil {
-			break
-		}
-		fmt.Print(string(buff[0:n]))
-		fmt.Println()
+		// log.Printf("Got %d bytes: %x\n", n, packet[:n])
+
+		parsedIpProtocol := protocols.ParseIPHeader(packet[:n])
+		destIP := parsedIpProtocol.DestIP
+		ip := net.IPv4(byte(destIP>>24), byte(destIP>>16), byte(destIP>>8), byte(destIP)).String()
+		fmt.Println(ip)
 	}
 }
