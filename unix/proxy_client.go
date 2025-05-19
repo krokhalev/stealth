bae5de5a137d186630adfd45cd56a72cea6e5307
krokhalev <cramush@gmail.com>
2025-05-20 01:14:02 +0300

    Add recover handler, gin server + logging.

--- unix/proxy_client.go
+++ unix/proxy_client.go
@@ -3,19 +3,58 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 	"os/signal"
 	"sync"
 	"syscall"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	port = 12345
 )
 
 func main() {
+	logger := logrus.New()
+	var wg sync.WaitGroup
+
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Printf("[-] Panic caught: %v\n", r)
+			Cleanup(&wg, logger)
+		}
+	}()
+
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
-	var wg sync.WaitGroup
-	go Init(ctx, &wg)
+	initDone := make(chan struct{}, 1)
+	errChan := make(chan error, 1)
+	go Init(ctx, &wg, initDone, logger, errChan)
+	<-initDone
+	logger.Info("[+] Init done")
 
-	<-ctx.Done()
+	client := gin.New()
+	client.Use(gin.LoggerWithWriter(logger.Writer(), "/status", "/metrics", "/health"), gin.Recovery())
+	client.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]string{"message": "pong"})
+	})
+	go func() {
+		err := client.Run("localhost:" + fmt.Sprint(port))
+		if err != nil {
+			errChan <- fmt.Errorf("failed to run client: %v", err)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		logger.Info("[!] Shutting down gracefully")
+	case err := <-errChan:
+		logger.Errorf("[-] Client exited with error: %v", err)
+		Cleanup(&wg, logger)
+	}
+
 	wg.Wait()
-	fmt.Printf("[!] Shutting down gracefully\n")
 }
