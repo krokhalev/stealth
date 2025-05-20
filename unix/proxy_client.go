b1293f3a18cdefa97406db654250a563b3f7cc2f
krokhalev <cramush@gmail.com>
2025-05-21 01:11:59 +0300

    Add ProxyClient struct.

--- unix/proxy_client.go
+++ unix/proxy_client.go
@@ -12,49 +12,65 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const (
-	port = 12345
-)
+type ProxyClient struct {
+	Port         int                `json:"port"`
+	Logger       *logrus.Logger     `json:"-"`
+	WaitGroup    *sync.WaitGroup    `json:"-"`
+	InitChan     chan struct{}      `json:"-"`
+	ErrChan      chan error         `json:"-"`
+	Ctx          context.Context    `json:"-"`
+	Cancel       context.CancelFunc `json:"-"`
+	ProcessNames []string           `json:"process_names"`
+}
+
+func NewProxyClient() *ProxyClient {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	return &ProxyClient{
+		Port:         12345,
+		Logger:       logrus.New(),
+		WaitGroup:    new(sync.WaitGroup),
+		InitChan:     make(chan struct{}, 1),
+		ErrChan:      make(chan error, 1),
+		Ctx:          ctx,
+		Cancel:       cancel,
+		ProcessNames: []string{"discord", "brave"},
+	}
+}
 
 func main() {
-	logger := logrus.New()
-	var wg sync.WaitGroup
+	PC := NewProxyClient()
+	defer PC.Cancel()
+	PC.Start()
+}
 
-	defer func() {
-		if r := recover(); r != nil {
-			logger.Printf("[-] Panic caught: %v\n", r)
-			Cleanup(&wg, logger)
-		}
-	}()
+func (PC *ProxyClient) Start() {
+	go PC.Init()
+	<-PC.InitChan
+	PC.Logger.Info("[+] Init done")
 
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
+	go PC.ginServer()
 
-	initDone := make(chan struct{}, 1)
-	errChan := make(chan error, 1)
-	go Init(ctx, &wg, initDone, logger, errChan)
-	<-initDone
-	logger.Info("[+] Init done")
+	select {
+	case <-PC.Ctx.Done():
+		PC.Logger.Info("[!] Shutting down gracefully")
+	case err := <-PC.ErrChan:
+		PC.Logger.Errorf("[-] Client exited with error: %v", err)
+		PC.Cleanup()
+	}
 
+	PC.WaitGroup.Wait()
+}
+
+func (PC *ProxyClient) ginServer() {
 	client := gin.New()
-	client.Use(gin.LoggerWithWriter(logger.Writer(), "/status", "/metrics", "/health"), gin.Recovery())
+	client.Use(gin.LoggerWithWriter(PC.Logger.Writer(), "/status", "/metrics", "/health"), gin.Recovery())
 	client.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, map[string]string{"message": "pong"})
 	})
 	go func() {
-		err := client.Run("localhost:" + fmt.Sprint(port))
+		err := client.Run("localhost:" + fmt.Sprint(PC.Port))
 		if err != nil {
-			errChan <- fmt.Errorf("failed to run client: %v", err)
+			PC.ErrChan <- fmt.Errorf("failed to run client: %v", err)
 		}
 	}()
-
-	select {
-	case <-ctx.Done():
-		logger.Info("[!] Shutting down gracefully")
-	case err := <-errChan:
-		logger.Errorf("[-] Client exited with error: %v", err)
-		Cleanup(&wg, logger)
-	}
-
-	wg.Wait()
 }
